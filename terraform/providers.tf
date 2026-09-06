ebc8eb8f2572855069badaa87ac0cf071f134733
Gopal Khatri <35285671+khatrigopal@users.noreply.github.com>
2026-09-06 14:10:06 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,7 @@
 provider "oci" {
-  auth   = "ResourcePrincipal"
-  region = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = var.private_key
+  region       = var.region
 }
