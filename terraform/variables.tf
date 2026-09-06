40fea703aeddf9e7b9dfdd7dc02cd015760f7ce1
Gopal Khatri <35285671+khatrigopal@users.noreply.github.com>
2026-09-06 14:11:32 +0530

    Add OCI sensitive variables and update descriptions
    Added sensitive variables for OCI credentials and updated descriptions for existing variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,34 @@
+variable "tenancy_ocid" {
+  description = "OCI Tenancy OCID"
+  type        = string
+  sensitive   = true
+}
+
+variable "user_ocid" {
+  description = "OCI User OCID"
+  type        = string
+  sensitive   = true
+}
+
+variable "fingerprint" {
+  description = "OCI API Key Fingerprint"
+  type        = string
+  sensitive   = true
+}
+
+variable "private_key" {
+  description = "OCI API Private Key"
+  type        = string
+  sensitive   = true
+}
+
 variable "region" {
-  description = "OCI region"
+  description = "OCI Region"
   type        = string
   default     = "ap-mumbai-1"
 }
 
 variable "compartment_ocid" {
-  description = "OCI compartment OCID"
+  description = "OCI Compartment OCID"
   type        = string
 }
